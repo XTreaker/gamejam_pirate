546ee458e548b467652dc303d4a54c6fc1804624
XTreaker <eugen.andrei12@gmail.com>
2025-01-23 13:07:12 +0200

    Climbing slopes

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -3,20 +3,44 @@ using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
-    flaot gravity = -20;
-    Vector3 velocity;
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded= .1f;
+    float moveSpeed = 5f;
 
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
 
     Controller2D controller;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         controller = GetComponent<Controller2D>();
+
+        gravity = -(2*jumpHeight) / Mathf.Pow(timeToJumpApex,2);
+        jumpVelocity = Mathf.Abs(gravity)*timeToJumpApex;
+        print("Gravity:" + gravity + " Jump velocity: " + jumpVelocity);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity  * Time.deltaTime);
     }
